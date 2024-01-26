92c00572b142e7736de34fd74e33f62feaebc9fd
unchin <isguoyq@gamil.com>
2024-01-26 18:18:53 +0800

    init

---
+++ src/main/java/com/airlines/AirlinesSyncApplication.java
@@ -0,0 +1,13 @@
+package com.airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinesSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/airlines/AirlinesSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
