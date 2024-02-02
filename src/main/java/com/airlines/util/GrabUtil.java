26df3f581ec58cca20c896918133b9dbeb834500
unchin <isguoyq@gamil.com>
2024-02-02 13:32:12 +0800

    update

--- src/main/java/com/airlines/util/GrabUtil.java
+++ src/main/java/com/airlines/util/GrabUtil.java
@@ -18,23 +18,39 @@ import java.util.List;
 public class GrabUtil {
 
     public static String getPageSource(String url) throws InterruptedException {
-        //设置chrome选项
-        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");//开启无头模式
-        //禁止gpu渲染
-        options.addArguments("--disable-gpu");
-        //关闭沙盒模式
-        options.addArguments("–-no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        //禁止加载图片
-        options.addArguments("--blink-settings=imagesEnabled=false");
-
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = getWebDriver();
         driver.get(url);
-
-        //设置5秒,全局寻找元素的等待时间
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+        mockClick(driver);
+
+        // 抓取当天的数据
+        grab(driver);
+
+
+        // 循环10次
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(1000);
+            WebElement nextDay = driver.findElement(By.cssSelector("[class = 'air-flight-slide swiper-slide swiper-slide-next']"));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", nextDay);
+            Thread.sleep(3000);
+            grab(driver);
+        }
+
+//        // 获取第二天的数据
+//        Thread.sleep(1000);
+//        WebElement nextDay = driver.findElement(By.cssSelector("[class = 'air-flight-slide swiper-slide swiper-slide-next']"));
+//        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", nextDay);
+//        Thread.sleep(3000);
+//        grab(driver);
+
+
+        String pageSource = driver.getPageSource();
+        driver.quit();
+        return pageSource;
+    }
+
+    private static void mockClick(WebDriver driver) throws InterruptedException {
         // 单程
         driver.findElement(By.xpath("//li[@data-triptype='OW']")).click();
 
@@ -79,6 +95,7 @@ public class GrabUtil {
         // 搜索
         Thread.sleep(1000);
         driver.findElement(By.id("searchFlight")).click();
+        Thread.sleep(3000);
 
         // 最大尝试次数
         int maxAttempts = 15;
@@ -100,34 +117,65 @@ public class GrabUtil {
                 log.info("第" + attempts + "次查询航班信息");
             }
         }
-        if (attempts == maxAttempts) {
-            log.error("超过最大尝试次数");
-            return "";
-        }
-
-        grab(driver);
-
-
-        // 获取第二天的数据
-        WebElement nextDay = driver.findElement(By.cssSelector("[class = 'air-flight-slide swiper-slide swiper-slide-next']"));
-        nextDay.click();
-
-
-        String pageSource = driver.getPageSource();
-        driver.quit();
-        return pageSource;
     }
 
-    private static void grab(WebDriver driver) {
+    private static WebDriver getWebDriver() {
+        //设置chrome选项
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");//开启无头模式
+        //禁止gpu渲染
+//        options.addArguments("--disable-gpu");
+        //关闭沙盒模式
+//        options.addArguments("–-no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+        //禁止加载图片
+//        options.addArguments("--blink-settings=imagesEnabled=false");
+
+        WebDriver driver = new ChromeDriver(options);
+        return driver;
+    }
+
+    private static void grab(WebDriver driver) throws InterruptedException {
         // 航班日期报价列表
         WebElement airFlightList = driver.findElement(By.className("air-flight-list"));
-        WebElement active = airFlightList.findElement(By.cssSelector("[class = 'air-flight-slide swiper-slide swiper-slide-active active']"));
-        String date = active.findElement(By.className("date")).getText();
-        String unit = active.findElement(By.className("unit")).getText();
-        String priceTxt = active.findElement(By.className("price_txt")).getText();
-        String priceStr = StrUtil.cleanBlank(priceTxt).replaceAll(",", "");
-        int price = Integer.parseInt(priceStr);
-        log.info("日期：" + date + " " + unit + " " + price);
+
+        // 最大尝试次数
+        int maxAttempts = 15;
+        int attempts = 0;
+        while (attempts < maxAttempts) {
+
+            try {
+                // 尝试找到元素
+                WebElement active = driver.findElement(By.cssSelector("[class = 'air-flight-slide swiper-slide swiper-slide-active active']"));
+                String date = active.findElement(By.className("date")).getText();
+                String unit = active.findElement(By.className("unit")).getText();
+                String priceTxt = active.findElement(By.className("price_txt")).getText();
+                String priceStr = StrUtil.cleanBlank(priceTxt).replaceAll(",", "");
+                int price = Integer.parseInt(priceStr);
+                log.info("日期：" + date + " " + unit + " " + price);
+                break;
+            } catch (org.openqa.selenium.NoSuchElementException e) {
+                // 元素未找到的异常处理:如果是卡在页面加载那一步，就需要刷新并且重新点击下一天的按钮
+
+//                WebElement loadingPage = driver.findElement(By.className("loading_page"));
+//                if (loadingPage.isDisplayed()) {
+//
+//                }
+
+
+                // 重新点击
+                Thread.sleep(1000);
+                WebElement activeNot = driver.findElement(By.cssSelector("[class = 'air-flight-slide swiper-slide swiper-slide-active']"));
+//                activeNot.click();
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click();", activeNot);
+
+
+                // 增加尝试次数
+                attempts++;
+                log.info("第" + attempts + "次查询第二天的航班信息");
+            }
+        }
+
 
         // 机票信息列表
         WebElement fareList = driver.findElement(By.className("fare-list"));
@@ -149,8 +197,8 @@ public class GrabUtil {
         log.info("出发时间：" + departureDate + " " + departureTimeText);
 
         // 持续时间
-        WebElement duration = listSummary.findElement(By.cssSelector("[class = 'moving-time']"));
-        String durationText = duration.getText();
+        WebElement durationTotal = listSummary.findElement(By.cssSelector("[class = 'moving-time']"));
+        String durationText = durationTotal.getText();
         log.info("持续时间：" + durationText);
 
         // 结束时间
@@ -171,8 +219,29 @@ public class GrabUtil {
             String remainingSeat = gradeBag.findElement(By.cssSelector("[class = 'remaining-seat']")).getText();
             log.info("剩余座位：" + remainingSeat);
         });
-    }
 
+        // 中转详情
+        Thread.sleep(1000);
+        WebElement detail = listSummary.findElement(By.cssSelector("[onclick = 'openConnectSection(this);']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", detail);
+        Thread.sleep(1000);
+        WebElement flightList = driver.findElement(By.className("flight-time__list"));
+        List<WebElement> takeoffList = flightList.findElements(By.cssSelector("[class = 'flight-time__list-item']"));
+        for (WebElement takeoff : takeoffList) {
+            String takeoffCity = takeoff.findElement(By.className("title")).getText();
+            String takeoffStation = takeoff.findElement(By.className("title-code")).getText();
+            WebElement info = takeoff.findElement(By.className("info"));
+            List<WebElement> spans = info.findElements(By.cssSelector("span"));
+            String takeoffTime = spans.get(0).getText();
+            String flightNumber = spans.get(1).getText();
+            String duration = takeoff.findElement(By.className("time")).getText();
+            log.info("起飞时间：" + takeoffTime + " " + flightNumber + " " + duration);
+        }
+
+        // 关闭详情
+        List<WebElement> closes = driver.findElements(By.className("modal__close"));
+        closes.get(closes.size()-1).click();
+    }
 
     public static void main(String[] args){
         DateTime now = DateTime.now();
